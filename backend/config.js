d5107979b8a9c99728cd880655913f6756145159
Shobhit Rastogi <rshobhit255@gmail.com>
2024-08-16 14:07:19 +0530

    git init

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://ironman:ironman@cluster0.e9zvc5m.mongodb.net/bookstore';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
